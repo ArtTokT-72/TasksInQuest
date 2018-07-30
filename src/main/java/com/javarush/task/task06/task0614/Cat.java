8ce3ee694a3d32cfe5083e9ced754a8d4807cfd3
Олег <tokt72@gmail.com>
2018-07-30 22:42:02 +0300

    Added some packages

---
+++ src/main/java/com/javarush/task/task06/task0614/Cat.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task06.task0614;
+
+import java.util.ArrayList;
+
+/* 
+Статические коты
+*/
+
+public class Cat {
+    //напишите тут ваш код
+    public static ArrayList<Cat> cats = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            Cat cat = new Cat();
+            Cat.cats.add(cat);
+        }
+        printCats();
+    }
+
+    public static void printCats() {
+        for (int j = 0; j < cats.size(); j++) {
+            System.out.println(cats.get(j));
+        }
+    }
+}
