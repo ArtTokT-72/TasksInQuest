f7314556c63b578c6218e9a5d5597d278a527b6a
Олег <tokt72@gmail.com>
2018-10-09 08:47:59 +0300

    Added some classes

---
+++ src/main/java/com/javarush/task/task08/task0826/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task08.task0826;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Пять победителей
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+
+    public static void sort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int newElement = array[i];
+            int count = i - 1;
+
+            while (count >= 0 && array[count] < newElement) {
+                array[count + 1] = array[count];
+                count--;
+            }
+
+            array[count + 1] = newElement;
+        }
+    }
+}
